2f2f9bad8bd679fb482c1c3e5c5f74c8b6ce6de5
Glyphack <sh.hooshyari@gmail.com>
2024-10-04 19:52:04 +0200

    First stage

--- app/main.c
+++ app/main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 int main() {
-  // Uncomment this block to pass the first stage
-  // printf("$ ");
-  // fflush(stdout);
+  printf("$ ");
+  fflush(stdout);
 
-  // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
   return 0;
